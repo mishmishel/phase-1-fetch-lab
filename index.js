b75335f7167f481dcc23c3507779ae1a6cbebf74
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-05 16:13:17 +1000

    commit tree 7241790edfe46432768f3252eb82b63544fa2024

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books") 
+    .then(function(resp) {
+      if(!resp.ok) {
+        throw new Error('Network response was not OK');
+      }
+    return resp.json();
+    })
+    .then(function(json) {
+      renderBooks(json);
+    })
+    .catch(function(err) {
+      console.log('Error:', err);
+    })
+} 
+
+fetchBooks()
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
